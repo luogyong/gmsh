f9c99e8c27c730f7c6db330d2def28fee25e856c
marsic <marsic@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2014-05-22 12:48:30 +0000

    Cleaning cavity_haroche_sf.py
    git-svn-id: https://geuz.org/svn/gmsh/trunk@18618 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/small_fem/tests/haroche/cavity_haroche_sf.py
+++ projects/small_fem/tests/haroche/cavity_haroche_sf.py
@@ -1,86 +1,91 @@
 import os
-import scipy as sc
-import scipy.io as sio
+import time
 import numpy as np
-import pylab as pl
-import sys
-pi    = np.pi
-# os.system('rm '+str_path+'/*.pos')
-# os.system('rm '+str_path+'/*.dat')
-# os.system('rm '+str_path+'/*.out')
-# os.system('rm '+str_path+'/*.txt')
-# os.system('rm '+str_path+'/*.pre')
-# os.system('rm '+str_path+'/*.res')
 
+## Physical / Math constant ##
+pi       = np.pi
+nm       = 1.e-9
+epsilon0 = 8.854187817e-3 * nm
+mu0      = 400. * pi * nm
+cel      = 1.0 / (np.sqrt(epsilon0 * mu0))
 
-#### your configuration (nproc is your number of available processors)
-str_gmsh_path    = '~/programs/gmsh/Gmsh-2.8.4-stable.app/Contents/MacOS/'
-str_getdp_path   = '~/programs/getdp/getdp_build_svn/'
-str_mesh_filename = 'haroche_mesh.msh'
-str_pro_filename  = 'cavity_haroche.pro'
-
-#### scaling the problem (*10^12)
-nm	     = 1.e-9
-epsilon0 = 8.854187817e-3*nm
-mu0      = 400.*pi*nm
-cel      = 1.0/(np.sqrt(epsilon0 * mu0))
-
-#### mesh parameters : MeshElementSize = lambda/(paramaille*n)
-lambda_m        = 5.8e-3
+## Mesh parameters : MeshElementSize = lambda / (paramaille * n) ##
+mesh_filename   = 'haroche_mesh.msh'
 refinement      = 3
-paramaille_air  = lambda_m/refinement
-paramaille_pml  = lambda_m/refinement
-paramaille_mir  = lambda_m/refinement
+lambda_m        = 5.8e-3
 
-#### opto-geometric parameters
-#lambda_vp = 5.8e-3
-#eig_target= (2.*np.pi*cel/lambda_vp)**2
-freq_target = 51.099e9
-lambda_vp = cel/freq_target
-eig_target= (2.*np.pi*cel/lambda_vp)**2
+paramaille_air  = lambda_m / refinement
+paramaille_pml  = lambda_m / refinement
+paramaille_mir  = lambda_m / refinement
 
-neig = 70
+## Solver Parameters ##
+neig      = 10
 geo_order = 2
-fem_order = 4
+fem_order = 0
 
-#### write all above variables in a text file, used as an input to both gmsh and getdp
-par_gmsh_getdp =open('parameters_gmsh_getdp.dat', 'w')
-par_gmsh_getdp.write('nm      		 = %3.15e;\n'%(nm))
-par_gmsh_getdp.write('epsilon0  	 = %3.15e;\n'%(epsilon0))
-par_gmsh_getdp.write('mu0  		     = %3.15e;\n'%(mu0))
-par_gmsh_getdp.write('cel    		 = %3.15e;\n'%(cel))
-par_gmsh_getdp.write('lambda_vp      = %3.15e;\n'%(lambda_vp))
-par_gmsh_getdp.write('lambda_m       = %3.15e;\n'%(lambda_m))
-par_gmsh_getdp.write('paramaille_air = %3.15e;\n'%(paramaille_air))
-par_gmsh_getdp.write('paramaille_pml = %3.15e;\n'%(paramaille_pml))
-par_gmsh_getdp.write('paramaille_mir = %3.15e;\n'%(paramaille_mir))
-par_gmsh_getdp.write('eig_target     = %3.15e;\n'%(eig_target))
-par_gmsh_getdp.write('neig       	 = %d    ;\n'%(neig))
+## Eigen Problem Shift ##
+freq_target = 51.099e9
+lambda_vp   = cel / freq_target
+eig_target  = (2. * pi * cel / lambda_vp)**2
+
+## Start ##
+start = time.time()
+
+print '## Haroche'
+print ' -- Geo order : %d' %geo_order
+print ' -- Refinement: %d' %refinement
+print ' -- FEM order : %d' %fem_order
+print ' -- N. Eigen  : %d' %neig
+print ' -- Shift     : %e' %eig_target
+print '                  '
+
+## Write Gmsh and GetDP data file ##
+par_gmsh_getdp = open('parameters_gmsh_getdp.dat', 'w')
+
+par_gmsh_getdp.write('nm             = %3.15e;\n' %(nm))
+par_gmsh_getdp.write('epsilon0       = %3.15e;\n' %(epsilon0))
+par_gmsh_getdp.write('mu0            = %3.15e;\n' %(mu0))
+par_gmsh_getdp.write('cel            = %3.15e;\n' %(cel))
+par_gmsh_getdp.write('lambda_vp      = %3.15e;\n' %(lambda_vp))
+par_gmsh_getdp.write('lambda_m       = %3.15e;\n' %(lambda_m))
+par_gmsh_getdp.write('paramaille_air = %3.15e;\n' %(paramaille_air))
+par_gmsh_getdp.write('paramaille_pml = %3.15e;\n' %(paramaille_pml))
+par_gmsh_getdp.write('paramaille_mir = %3.15e;\n' %(paramaille_mir))
+par_gmsh_getdp.write('eig_target     = %3.15e;\n' %(eig_target))
+par_gmsh_getdp.write('neig           = %d    ;\n' %(neig))
+
 par_gmsh_getdp.close()
 
-### define strings to be "oscommanded"
-str_getdp1        = str_getdp_path+'getdp '+str_pro_filename+' -pre all -msh  '+str_mesh_filename+' -cal -pos postop_eigenvectors_full -v 50 -bin'
-str_slepc_options =  ' -eps_monitor -eps_view \
-                -eps_type krylovschur \
-               -st_ksp_type preonly \
-               -st_pc_type lu \
-               -st_pc_factor_mat_solver_package mumps \
-               -eps_max_it 100 \
-               -eps_mpd 400 \
-               -eps_target '+ "%3.5e" % eig_target +' \
-               -eps_target_real \
-               -eps_nev '+"%d" % (neig)
-               # \
-               #-v 0'
-# ### calling gmsh : mesh & save mesh!
-os.system(str_gmsh_path+'gmsh geometry_haroche_realistic.geo -3 -order' + str(geo_order) + ' -o '+str_mesh_filename)
-# ### calling small_fem : preprocess and solve, using mpi if required
-os.system('har -msh '+str_mesh_filename+' -o '+str(fem_order)+' -n '+str(neig)+' -shifht '+"%3.5e" % eig_target+' -solver -eps_monitor')
-os.rename('haroche.msh','haroche_femorder_+'str(fem_order)+'_geoorder_'+str(geo_order)+'_paramaille_'+str(refinement)+'.msh')
-os.rename('eigenValues.txt','eigenValues_femorder_+'str(fem_order)+'_geoorder_'+str(geo_order)+'_paramaille_'+str(refinement)+'.txt')
+## Calling Gmsh ##
+print '## Meshing'
+os.system('gmsh geometry_haroche_realistic.geo -3 -order %d ' %geo_order + \
+          '-o ' + mesh_filename + ' -v 3')
 
+## Calling small_fem ##
+print '## Simulating'
+os.system('har'  + \
+          ' -msh ' + mesh_filename    + \
+          ' -o %d'        %fem_order  + \
+          ' -n %d'        %neig       + \
+          ' -shift %3.5e' %eig_target + \
+          ' -solver -eps_monitor')
 
+## Renaming Output ##
+os.rename('haroche.msh', \
+          'haroche_femorder_%d' %fem_order  + \
+          '_geoorder_%d'        %geo_order  + \
+          '_paramaille_%d'      %refinement +'.msh')
 
+os.rename('eigenValues.txt', \
+          'eigenValues_femorder_%d' %fem_order  + \
+          '_geoorder_%d'            %geo_order  + \
+          '_paramaille_%d'          %refinement +'.txt')
+
+## Done ##
+stop = time.time()
+print '## Done in %e' %(stop - start) + ' [s]'
+
+## Post Pro ##
 #vp_real = np.loadtxt('./EigenValuesReal.txt',usecols=[5])
 #vp_imag = np.loadtxt('./EigenValuesImag.txt',usecols=[5])
 #print 'EigenFreq (GHz)', 2e9*np.pi*vp_real.transpose()
