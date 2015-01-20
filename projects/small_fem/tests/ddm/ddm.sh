a4bee0236ffe9ef5bb12ddcc4fb7b645a5152de0
marsic <marsic@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2015-01-20 13:45:32 +0000

    Preparing for circle ddm convergence
    git-svn-id: https://geuz.org/svn/gmsh/trunk@20288 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/small_fem/tests/ddm/ddm.sh
+++ projects/small_fem/tests/ddm/ddm.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-## DATA
-THREADS=2
+## Binaries
+BINREF='disc'
+BINDDM='cir'
+
+## Data
+THREADS=1
 TYPE='vector'
-DDM='emda'
-DOM=2
+NDOM=4
+DOM=0
+DDM='osrc'
 MSH='./mesh_all.msh'
 REF='./fine.msh'
-OR=4
-OV=4
-OB=4
+OR=3
+OV=3
+OB=3
 K=5
 
 ## Useful
-DOMMinus=$(bc <<< $DOM'-1')
+NDOMMinus=$(bc <<< $NDOM'-1')
 
 ## Reference Solution
 echo '#### Reference ####'
-waveg -msh $MSH -k $K -n $DOM -type $TYPE -interp $REF -o $OR -name 'ref'
+$BINREF -msh $MSH -k $K -n $NDOM -type $TYPE -interp $REF -o $OR -name 'ref'
 
 ## DDM
 for v in $(seq 1 $OV);
@@ -27,16 +32,16 @@ do
         NAME=$TYPE'_'$DDM'_'$v'_'$b
         echo '#### '$NAME' ####'
 
-        OMP_NUM_THREADS=$THREADS mpirun -np $DOM ddm -msh $MSH -k $K \
-            -max 1000 -ddm $DDM \
+        OMP_NUM_THREADS=$THREADS mpirun -np $NDOM $BINDDM -msh $MSH -k $K \
+            -max 250 -ddm $DDM \
             -pade 4 -ck 0 -chi 0 -lc 0.06 -type $TYPE -ov $v -ob $b \
             -name $NAME -interp $REF -hist $NAME'.hist' -solver -ksp_rtol 1e-9
 
         ## L2 Error
-        octave -q l2.m $NAME'0.dat' 'ref0.dat' > $NAME'.tmp'
+        octave -q l2.m $NAME$DOM'.dat' 'ref'$DOM'.dat' > $NAME'.tmp'
 
         ## Clear
-        for i in $(seq 0 $DOMMinus);
+        for i in $(seq 0 $NDOMMinus);
         do
             rm $NAME$i'.dat'
         done
@@ -44,7 +49,7 @@ do
 done
 
 ## Clear Reference
-for i in $(seq 0 $DOMMinus);
+for i in $(seq 0 $NDOMMinus);
 do
     rm 'ref'$i'.dat'
 done
--- projects/small_fem/tests/ddm/ref.sh
+++ projects/small_fem/tests/ddm/ref.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-## DATA
+## Binaries
+BINREF='disc'
+
+## Data
 TYPE='vector'
-DOM=2
+NDOM=4
+DOM=0
 MSH='./mesh_all.msh'
 REF='./fine.msh'
-OR=4
+OR=3
 K=5
 
 ## Useful
-DOMMinus=$(bc <<< $DOM'-1')
+NDOMMinus=$(bc <<< $NDOM'-1')
 
 ## Reference Solution
 echo '#### Reference ####'
-waveg -msh $MSH -k $K -n $DOM -type $TYPE -interp $REF -o $OR -name 'ref'
+$BINREF -msh $MSH -k $K -n $NDOM -type $TYPE -interp $REF -o $OR -name 'ref'
 
 ## Lower order solution
 for o in $(seq 1 $OR);
@@ -21,18 +25,18 @@ do
     NAME='order_'$o
     echo '#### '$NAME' ####'
 
-    waveg -msh $MSH -k $K -n $DOM -type $TYPE -interp $REF -o $o -name $NAME
-    octave -q l2.m $NAME'0.dat' 'ref0.dat' > $NAME'.tmp'
+    $BINREF -msh $MSH -k $K -n $NDOM -type $TYPE -interp $REF -o $o -name $NAME
+    octave -q l2.m $NAME$DOM'.dat' 'ref'$DOM'.dat' > $NAME'.tmp'
 
     ## Clear
-    for i in $(seq 0 $DOMMinus);
+    for i in $(seq 0 $NDOMMinus);
     do
         rm $NAME$i'.dat'
     done
 done
 
 ## Clear Reference
-for i in $(seq 0 $DOMMinus);
+for i in $(seq 0 $NDOMMinus);
 do
     rm 'ref'$i'.dat'
 done
