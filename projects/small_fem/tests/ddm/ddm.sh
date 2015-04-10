0715ff45bf53b2fad3e978b7b14a4a31dd21bd3b
marsic <marsic@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2015-04-10 15:51:26 +0000

    oops
    git-svn-id: https://geuz.org/svn/gmsh/trunk@21130 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/small_fem/tests/ddm/ddm.sh
+++ projects/small_fem/tests/ddm/ddm.sh
@@ -9,61 +9,61 @@ THREADS=2
 TYPE='vector'
 NDOM=2
 DDM='osrc'
-NMSH=2
+NMSH=3
 MSH='./guide3d_'
-REF='./guide3d_4.msh'
-OR=2
-OV=2
-OB=2
+REF='./guide3d_16.msh'
+OR=3
+OV=3
+OB=3
 K=10
 
 ## Useful
 NDOMMinus=$(bc <<< $NDOM'-1')
 
 ## Reference Solution
-#echo '#### Reference ####'
-#MYMESH=$MSH$(bc <<< 2^$NMSH)'.msh'
-#$BINREF -msh $MYMESH -k $K -n $NDOM -type $TYPE -o $OR -sigma 0 \
-#        -interp $REF -name 'ref'
-#
-### DDM
-#for m in $(seq 1 $NMSH)
-#do
-#    MYMESH=$MSH$(bc <<< 2^$m)'.msh'
-#    for v in $(seq 1 $OV)
-#    do
-#        for b in $(seq 1 $OB)
-#        do
-#            NAME=$TYPE'_'$DDM'_'$m'_'$v'_'$b
-#            echo '#### '$NAME' ####'
-#
-#            OMP_NUM_THREADS=$THREADS
-#            mpirun -np $NDOM $BINDDM -msh $MYMESH -k $K \
-#                   -max 250 -ddm $DDM \
-#                   -pade 4 -ck 0 -chi 0 -lc 0.06 -type $TYPE -ov $v -ob $b \
-#                   -name $NAME -interp $REF -hist $NAME'.hist' \
-#                   -solver -ksp_rtol 1e-9
-#
-#            ## L2 Error
-#            for d in $(seq 0 $NDOMMinus)
-#            do
-#            octave -q l2.m $NAME$d'.dat' 'ref'$d'.dat' > $NAME'_'$d'.tmp'
-#            done
-#
-#            ## Clear
-#            for d in $(seq 0 $NDOMMinus)
-#            do
-#                rm $NAME$d'.dat'
-#            done
-#        done
-#    done
-#done
-#
-### Clear Reference
-#for d in $(seq 0 $NDOMMinus);
-#do
-#    rm 'ref'$d'.dat'
-#done
+echo '#### Reference ####'
+MYMESH=$MSH$(bc <<< 2^$NMSH)'.msh'
+$BINREF -msh $MYMESH -k $K -n $NDOM -type $TYPE -o $OR -sigma 0 \
+        -interp $REF -name 'ref'
+
+## DDM
+for m in $(seq 1 $NMSH)
+do
+    MYMESH=$MSH$(bc <<< 2^$m)'.msh'
+    for v in $(seq 1 $OV)
+    do
+        for b in $(seq 1 $OB)
+        do
+            NAME=$TYPE'_'$DDM'_'$m'_'$v'_'$b
+            echo '#### '$NAME' ####'
+
+            OMP_NUM_THREADS=$THREADS
+            mpirun -np $NDOM $BINDDM -msh $MYMESH -k $K \
+                   -max 250 -ddm $DDM \
+                   -pade 4 -ck 0 -chi 0 -lc 0.06 -type $TYPE -ov $v -ob $b \
+                   -name $NAME -interp $REF -hist $NAME'.hist' \
+                   -solver -ksp_rtol 1e-9
+
+            ## L2 Error
+            for d in $(seq 0 $NDOMMinus)
+            do
+                octave -q l2.m $NAME$d'.dat' 'ref'$d'.dat' > $NAME'_'$d'.tmp'
+            done
+
+            ## Clear
+            for d in $(seq 0 $NDOMMinus)
+            do
+                rm $NAME$d'.dat'
+            done
+        done
+    done
+done
+
+## Clear Reference
+for d in $(seq 0 $NDOMMinus);
+do
+    rm 'ref'$d'.dat'
+done
 
 ## Grab every residual in a single file
 FILE=$TYPE'_'$DDM'_'$NMSH'_'$OV'_'$OB'.l2.dat'
