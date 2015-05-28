46ddd89e2afb28072386e8de46e73d290d0f8e2d
marsic <marsic@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2015-05-28 12:14:00 +0000

    Oops: Ddm 3D TM mode had wrong sign
    git-svn-id: https://geuz.org/svn/gmsh/trunk@21423 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/small_fem/simulation/Ddm.cpp
+++ projects/small_fem/simulation/Ddm.cpp
@@ -86,9 +86,9 @@ fullVector<Complex> fSourceVect(fullVector<double>& xyz){
   */
 
   // TMmn 3D
-  tmp(0) = E0                                  * sin(ky * y) * sin(kz * z);
-  tmp(1) = E0 * (-I * kx * ky) / (k*k - kx*kx) * cos(ky * y) * sin(kz * z);
-  tmp(2) = E0 * (-I * kx * kz) / (k*k - kx*kx) * sin(ky * y) * cos(kz * z);
+  tmp(0) = E0                                 * sin(ky * y) * sin(kz * z);
+  tmp(1) = E0 * (I * kx * ky) / (k*k - kx*kx) * cos(ky * y) * sin(kz * z);
+  tmp(2) = E0 * (I * kx * kz) / (k*k - kx*kx) * sin(ky * y) * cos(kz * z);
 
   return tmp;
 }
